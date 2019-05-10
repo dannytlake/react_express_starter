d3740dceb63980a0d5ff0c3c1292e02e8155c001
Danny Lake <dannylake@Dannys-MacBook-Pro.local>
2019-05-09 22:07:24 -0700

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
 
+
+class Card extends Component {
+  render() {
+    const style = {
+      margin: 20,
+      padding: 20,
+      color: 'white',
+      backgroundColor: this.props.color,
+    }
+
+    return (
+      <div style={style}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -12,7 +30,22 @@ class App extends Component {
           <h1 className="App-title">React Express Starter</h1>
         </header>
         <Customers />
+        <div>
+          <input type={'text'} defaultValue={'Type here!'} />
+          <select>
+            <option>A</option>
+            <option>B</option>
+          </select>
+          <img alt='h1' src={'//unsplash.it/200/300'} />
+        </div>
+
+        <div>
+          <Card color={'skyblue'}>Card 1</Card>
+          <Card color={'steelblue'}>Card 2</Card>
+        </div>
       </div>
+
+
     );
   }
 }
